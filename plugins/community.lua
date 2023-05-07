9d0ff91406cccd246a9bcb010a7eb8db1949472f
L1an <l1an10@foxmail.com>
2023-05-07 21:24:35 +0800

    some changes

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,3 +1,6 @@
+function GUI() return vim.g.neovide and false or true end
+
+
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
@@ -5,5 +8,24 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.gruvbox-baby" },
+
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
+
+  { import = "astrocommunity.scrolling.mini-animate", enabled = GUI() },
+  { import = "astrocommunity.scrolling.nvim-scrollbar" },
+
+  { import = "astrocommunity.syntax.hlargs-nvim" },
+  { import = "astrocommunity.test.neotest" },
+
+  { import = "astrocommunity.utility.transparent-nvim" },
+  { import = "astrocommunity.utility.noice-nvim" },
 }
