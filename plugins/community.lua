d2c47fe6f5e2ef58e78d1e030a4a572023906308
L1an <l1an10@foxmail.com>
2023-05-08 09:45:00 +0800

    used lualine

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,6 +7,7 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  { import = "astrocommunity.bars-and-lines.lualine-nvim" },
   { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
   { import = "astrocommunity.colorscheme.gruvbox-baby" },
 
